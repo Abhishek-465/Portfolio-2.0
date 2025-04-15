4bf5fa4a9207608ada3b40c79db5ffa8fc157053
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2025-04-15 11:08:39 +0530

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,261 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Experiencing the beauty of Electrical Engineering ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/electric.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm also an Astrophysics enthusiast.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "/space.jpg",
+  },
+  {
+    id: 3,
+    title: "Tech Stack",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2 flex justify-center",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "I love Programming!",
+    description: "Web-dev basic ML, DSA",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to collaborate?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Model of Neutron Star",
+    des: "Explore the wonders of the neutron stars with this project.",
+    img: "/neutron.jpg",
+    iconLists: ["/python.jpg", "/jupi.png","/numpy.png","/mat.png"],
+    link: "https://github.com/Abhishek-465/Neutron-Star-Model",
+    deploy: "https://github.com/Abhishek-465/Neutron-Star-Model",
+  },
+  {
+    id: 2,
+    title: "Power Quality Analysis ",
+    des: "This project focuses on analyzing the power quality of electrical signals drawn by non-linear loads using FFT & THD.",
+    img: "/fft.jpg",
+    iconLists: ["/matlab.png"],
+    link: "https://github.com/Abhishek-465/Power-Quality-Analysis",
+    deploy: "https://github.com/Abhishek-465/Power-Quality-Analysis",
+  },
+  {
+    id: 3,
+    title: "Satellite Orbit Simulator",
+    des: "This interactive web application allows you to simulate satellite orbits around Earth and display key parameters",
+    img: "/orbit.jpg",
+    iconLists: ["/html.png","/css.png","/js.png"],
+    link: "https://github.com/Abhishek-465/Satellite-orbit-simulator",
+    deploy: "https://github.com/Abhishek-465/Satellite-orbit-simulator",
+  },
+  {
+    id: 4,
+    title: "Obstacle Avoidance Car",
+    des: "This project involves designing and building a car that autonomously navigates around obstacles.",
+    img: "/arduino2.jpg",
+    iconLists: ["/ard.png"],
+    link: "#",
+    deploy: "#",
+  },
+  {
+    id: 5,
+    title: "Promptbook",
+    des: "Create and share all your AI prompts in this Web application.",
+    img: "/prompt.png",
+    iconLists: ["/next.svg", "/tail.svg", "/GCP.png"],
+    link: "https://github.com/Abhishek-465/Promptbook",
+    deploy:
+      "https://github.com/Abhishek-465/Promptbook",
+  },
+  {
+    id: 6,
+    title: "Street-Fighter-Samurai Game",
+    des: "A 2V2 fighting game for desktop. Based on the classic Street Fighter game.",
+    img: "/samurai.jpg",
+    iconLists: ["/html.png", "/css.png", "/js.png"],
+    link: "https://github.com/Abhishek-465/StreetFighterSamurai-2D-Game",
+    deploy: "https://github.com/Abhishek-465/StreetFighterSamurai-2D-Game",
+  },
+  {
+    id: 7,
+    title: "Navigo",
+    des: "Instantly search and know about the nearby important amenities of any place in the world.",
+    img: "/weather.png",
+    iconLists: ["/next.svg","/mern-stack.png", "/tail.svg", "/GCP.png"],
+    link: "https://github.com/Abhishek-465/Navigo",
+    deploy: "https://github.com/Abhishek-465/Navigo",
+  },
+
+
+  {
+    id: 8,
+    title: "Eveluna",
+    des: "EveLuna is a menstrual cycle tracking web app.",
+    img: "/eveluna.webp",
+    iconLists: ["/html.png","/css.png","/js.png", "/GCP.png"],
+    link: "https://github.com/Abhishek-465/Eveluna",
+    deploy: "https://github.com/Abhishek-465/Eveluna",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Abhishek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abhishek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, he is the ideal partner.",
+    name: "Ritwika Mahapatra",
+    title: "B-Tech CSE HITK",
+  },
+  {
+    quote:
+      "Collaborating with Abhishek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abhishek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, he is the ideal partner.",
+    name: "Ritwika Mahapatra",
+    title: "B-Tech CSE HITK",
+  },
+  {
+    quote:
+      "Collaborating with Abhishek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abhishek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, he is the ideal partner.",
+    name: "Ritwika Mahapatra",
+    title: "B-Tech CSE HITK",
+  },
+  {
+    quote:
+      "Collaborating with Abhishek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abhishek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, he is the ideal partner.",
+    name: "Ritwika Mahapatra",
+    title: "B-Tech CSE HITK",
+  },
+  {
+    quote:
+      "Collaborating with Abhishek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abhishek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, he is the ideal partner.",
+    name: "Ritwika Mahapatra",
+    title: "B-Tech CSE HITK",
+  },
+  {
+    quote:
+      "Collaborating with Abhishek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abhishek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, he is the ideal partner.",
+    name: "Ritwika Mahapatra",
+    title: "B-Tech CSE HITK",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "B.E Electrical Engineering",
+    desc: "Jadavpur University [2022-2026].",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 2,
+    title: "Research Intern",
+    desc: "Techno Electric & Engineering Company Ltd (TEECL)",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Research Intern",
+    desc: "IEEE-SS under [NIT Silchar]. Researched on Solar based charging in EVs.  ",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 4,
+    title: "Technical Team Member",
+    desc: "IEEE Jadavpur University Student Branch",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+export const honors = [
+  {
+    id: 1,
+    img: "/cert1.jpg",
+    title: "Hackathon Winner",
+    desc: "Issued by IEM and UEM · Mar 2024",
+  },
+  {
+    id: 2,
+    img: "/cert2.jpg",
+    title: "Top Contributor",
+    desc: "Issued by Kharagpur OS · Feb 24",
+  },
+  {
+    id: 3,
+    img: "/cert3.jpg",
+    title: "All-Round Performer 2021-22",
+    desc: "Issued by Telegraph School of Awards · Sep 2022",
+  },
+  {
+    id: 4,
+    img: "/cert5.jpg",
+    title: "Finalist in Circuistics",
+    desc: "By JUEE Convolution 9.0 Forum . Apr 2024",
+  },
+];
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Abhishek-465/",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/abhiii_1432?t=pLknF-hgkUtJfHKivfg_bA&s=09",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/abhishek-bhattacharjee-a91570256",
+  },
+];
